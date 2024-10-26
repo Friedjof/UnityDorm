655b6e066602f1baaedb7ef6cb4ab473d7eacedc
friedjof@noweck.info <friedjof@noweck.info>
2024-10-27 00:12:08 +0200

    fix: media path error

--- UnityDorm/settings.py
+++ UnityDorm/settings.py
@@ -156,6 +156,11 @@ else:
     STATICFILES_DIRS = []
     STATIC_ROOT = BASE_DIR / 'static'
 
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
